cf78e8607e7f699cb3a4d76fb7fb2c880e318f33
ApplePro <applepro@ApplePros-MacBook-Pro.local>
2020-11-08 21:38:17 +0530

    test serial execution

---
+++ parallel_test.sh
@@ -0,0 +1,4 @@
+python selenium_test.py dev "macOS Catalina" "Chrome" "75.0" &
+python selenium_test.py dev "macOS Catalina" "Firefox" "75.0" &
+sleep 2
+python selenium_test.py dev "macOS Catalina" "Safari" "13.0"
